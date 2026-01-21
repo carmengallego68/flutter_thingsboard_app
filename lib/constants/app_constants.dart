9864ca21392ac8dcc721d1c22509d675051e51c0
carmengallego68 <carmengallego68@gmail.com>
2026-01-21 10:32:16 +0100

    Add files via upload

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+abstract class ThingsboardAppConstants {
+  static const thingsBoardApiEndpoint = 'https://tabit-tecnologia.es:8443';
+  static const thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
+
+  /// Not for production (only for debugging)
+  static const thingsboardOAuth2AppSecret = 'Your app secret here';
+}
